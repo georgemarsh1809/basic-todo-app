e951af0343eccef8d22cedfbb0ecf80017c3f897
George Marsh <georgemarsh1809@gmail.com>
2025-01-02 14:05:16 +0000

    first commit

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,25 @@
+export default function TodoCard(props) {
+  const { children, handleDeleteTodos, index, handleEditTodos } = props;
+
+  return (
+    <div>
+      <li className='todoItem'>
+        {children}
+            <div className="actionsContainer">
+
+            <button onClick={()=>{
+              handleEditTodos(index)
+            }}>
+              <i className="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button onClick={()=> {
+              handleDeleteTodos(index)
+            }}>
+              <i className="fa-solid fa-trash-can"></i>
+            </button>
+
+            </div>
+      </li>
+    </div>
+  )
+}
